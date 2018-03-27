99b3532f852c563eb5637e3136bdd93409a13410
Mayra Jaimes <mtj26@georgetown.edu>
2018-03-27 16:10:38 -0500

    testing the reading function

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -4,7 +4,6 @@ const User = require('../src/user');
 describe('Creating records', () => {
   it('it saves a user', (done) => {
     const joe = new User({name: 'Joe'});
-    
     joe.save()
     .then (() => {
       //Checks if user has been saved successfully
