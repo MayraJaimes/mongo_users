007c6c3f7cce7bb95a6c3dfef98b4e0bd39d5bf1
Mayra Jaimes <mtj26@georgetown.edu>
2018-03-27 15:36:08 -0500

    running test to see if user is saved

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,9 +1,16 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
  
 mongoose.connect('mongodb://localhost/users_test');
-
 mongoose.connection
   .once ('open', () => console.log('Good to go!'))
   .on ('error', (error) => {
     console.log.warn('Warning', error);
   });
+
+//before running test we want to drop information
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    //Ready to run the next test!
+    done();
+  });
+});
