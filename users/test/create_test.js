9e36463ed7729c4fb1198500a168bfb949aa9e1d
Mayra Jaimes <mtj26@georgetown.edu>
2018-03-27 15:49:20 -0500

    enhanced connection to mongoose

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -2,9 +2,14 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating records', () => {
-  it('it saves a user', () => {
+  it('it saves a user', (done) => {
     const joe = new User({name: 'Joe'});
     
-    joe.save();
+    joe.save()
+    .then (() => {
+      //Checks if user has been saved successfully
+      assert(!joe.isNew);
+      done();
+    });
   });
 });
--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
- 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-  .once ('open', () => console.log('Good to go!'))
-  .on ('error', (error) => {
-    console.log.warn('Warning', error);
-  });
 
-//before running test we want to drop information
+//Using the ES6 implementation
+mongoose.Promise = global.Promise;
+ 
+//Run first test after connection has been made
+before((done) => {
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection
+    .once ('open', () => { done (); })
+    .on ('error', (error) => {
+      console.log.warn('Warning', error);
+    });
+});
+
+//Before running test we want to drop users
 beforeEach((done) => {
   mongoose.connection.collections.users.drop(() => {
     //Ready to run the next test!
